8dd7e481cb869044d664a5af5cf7f08bc79b6756
xploited11 <anish.timila@yahoo.com>
2015-11-05 21:13:33 -0600

    Update calc.s

--- calc.s
+++ calc.s
@@ -21,10 +21,9 @@ main:
     MOV R2, R8		            @ move return value R0 to argument register R1
     MOV R3, R9
     BL _compare
-    MOV R4, R0              @ move R0 to R1
-    BL _reg_dump
+    #MOV R1, R0              @ move R0 to R1
     LDR R0, =Printf_Output  @ R0 contains formatted string address
-    #BL  _printf             @ branch to print procedure with return
+    BL  _printf             @ branch to print procedure with return
     B   main                @ branch to exit procedure with no return
   
 _getchar:
