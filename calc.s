b8a7b002eca3d0bbb8aa741934455fe89bc6a5f3
xploited11 <anish.timila@yahoo.com>
2015-11-07 12:29:48 -0600

    Update calc.s

--- calc.s
+++ calc.s
@@ -21,6 +21,7 @@ main:
     MOV R2, R8		            @ move return value R0 to argument register R1
     MOV R3, R9
     BL _compare
+    BL _reg_dump
     MOV R1, R0                          @ move R0 to R1
     BL _reg_dump
     LDR R0, =Printf_Output              @ R0 contains formatted string address
