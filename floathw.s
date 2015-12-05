a6f0fa925fe045de8e333bc00cbc25df1fa9abd9
xploited11 <anish.timila@yahoo.com>
2015-12-04 23:15:56 -0600

    Update floathw.s

--- floathw.s
+++ floathw.s
@@ -12,19 +12,18 @@
     .func main
    
 main:
-
-    LDR R0, =val1           @ load variable address
+    BL  _scanf		            @ calls scanf for first operand
+    MOV R4, R0		            @ move value of R0 to register R1 
     VLDR S0, [R0]           @ load the value into the VFP register
-    
-    LDR R0, =val2           @ load variable address
+    FSITO<S/D> S0, R0
+    BL  _scanf		            @ calls scanf for first operand
+    MOV R5, R0		            @ move value of R0 to register R1 
     VLDR S1, [R0]           @ load the value into the VFP register
-    
+    FSITO<S/D> S1, R0
     VDIV.F32 S2, S0, S1     @ compute S2 = S0 * S1
-    
     VCVT.F64.F32 D4, S2     @ covert the result to double precision for printing
     VMOV R1, R2, D4         @ split the double VFP register into two ARM registers
     BL  _printf_result      @ print the result
-    
     B   _exit               @ branch to exit procedure with no return
    
 _exit:  
@@ -36,6 +35,16 @@ _exit:
     MOV R7, #1              @ terminate syscall, 1
     SWI 0                   @ execute syscall
 
+_scanf:
+    PUSH {LR}               @ pushes LR in stack since scanf call overwrites
+    SUB SP, SP, #4          @ make room on stack
+    LDR R0, =format_str     @ R0 contains address of format string
+    MOV R1, SP              @ move SP to R1 to store entry on stack
+    BL scanf                @ call scanf
+    LDR R0, [SP]            @ load value at SP into R0
+    ADD SP, SP, #4          @ restore the stack pointer
+    POP {PC}                @ pops PC
+
 _printf_result:
     PUSH {LR}               @ push LR to stack
     LDR R0, =result_str     @ R0 contains formatted string address
@@ -44,6 +53,7 @@ _printf_result:
 
 .data
 result_str:     .asciz      "Multiplication result = %f \n"
+format_str:     .asciz      "%d"
 exit_str:       .ascii      "Terminating program.\n"
 val1:           .float      3.14159
 val2:           .float      0.100
