2641b5f6f9efab73c1f0d4f46c26025fd93b9e78
xploited11 <anish.timila@yahoo.com>
2015-12-09 08:38:55 -0600

    Update test4.s

--- test4.s
+++ test4.s
@@ -1,66 +1,57 @@
-.global main
-.func main
-   
+	.global main
+	.func main
+
 main:
-    BL _prompt
-    BL _scanf
-    MOV R7,R0
-    BL _prompt
-    BL _scanf
-    MOV R8,R0
-    VLDR S0, [R7]
-    VLDR S1, [R8]
-    VDIV.F32 S2, S0, S1
-    VCVT.F64.F32 D4, S2
-    MOV R1,R7
-    MOV R2,R8
-    BL _printf
-    VMOV R1, R2, D4
-    BL _printf2
-    BL main               
-    
-    
-    
-_prompt:
-    MOV R7, #4              @ write syscall, 4
-    MOV R0, #1              @ output stream to monitor, 1
-    MOV R2, #23             @ print string length
-    LDR R1, =prompt_str     @ string at label prompt_str:
-    SWI 0                   @ execute syscall
-    MOV PC, LR              @ return
-    
+	BL _scanf
+	MOV R6, R0
+	BL _scanf
+	MOV R7, R0
+
+	#MOV R6, #2
+	#MOV R7, #2
+
+	VMOV S0, R6
+	VMOV S1, R7
+
+	MOV R1, R6
+	MOV R2, R7
+	BL _printf
+	
+	FSITOS S0, S0
+	FSITOS S1, S1
+
+	VDIV.F32 S2, S0, S1
+	VCVT.F64.F32 D4, S2
+
+	VMOV R1, R2, D4
+	BL _printf_result
+
+	B main	
+
+_printf:
+	PUSH {LR}
+	LDR R0, =printf_str
+	MOV R1, R1
+	BL printf
+	POP {PC}
+
+_printf_result:
+	PUSH {LR}
+	LDR R0, =float_str
+	BL printf
+	POP {PC}
 
 _scanf:
-    PUSH {LR}               @ store the return address
-    PUSH {R1}               @ backup regsiter value
-    LDR R0, =format_str     @ R0 contains address of format string
-    SUB SP, SP, #4          @ make room on stack
-    MOV R1, SP              @ move SP to R1 to store entry on stack
-    BL scanf                @ call scanf
-    LDR R0, [SP]            @ load value at SP into R0
-    ADD SP, SP, #4          @ remove value from stack
-    POP {R1}                @ restore register value
-    POP {PC}                @ restore the stack pointer and return
-    
-_printf:
-    PUSH {LR}               @ store the return address
-    LDR R0, =printf_str     @ R0 contains formatted string address
-    BL printf               @ call printf
-    POP {PC}                @ restore the stack pointer and return
-    
-_printf2:
-    PUSH {LR}               @ store the return address
-    LDR R0, =printf_str2     @ R0 contains formatted string address
-    BL printf               @ call printf
-    POP {PC}                @ restore the stack pointer and return
-   
-   
-   
-   
-.data
+	MOV R4, LR
+	SUB SP, SP, #4
+	LDR R0, =format_str
+	MOV R1, SP
+	BL scanf
+	LDR R0, [SP]
+	ADD SP, SP, #4
+	MOV PC, R4
 
-printf_str:           .asciz      "%d/%d = "
-printf_str2:          .asciz      "%f\n"
-prompt_str:           .asciz      "Please enter an integer: "
-format_str:           .asciz      "%d\n"
-exit_str:             .ascii      "Terminating program.\n"
+.data
+printf_str:	.asciz	"%d / %d = "
+float_str:	.asciz	"%f\n"
+format_str:	.asciz	"%d"
