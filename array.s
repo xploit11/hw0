dec1388d34f9860c30239ac8007e11d4d6626236
xploited11 <anish.timila@yahoo.com>
2015-12-11 16:08:44 -0600

    Update array.s

--- array.s
+++ array.s
@@ -65,9 +65,21 @@ _printf:
     BL printf               @ call printf
     POP {PC}                @ restore the stack pointer and return
    
+ _scanf:
+    PUSH {LR}               @ store LR since scanf call overwrites
+    SUB SP, SP, #4          @ make room on stack
+    LDR R0, =format_str     @ R0 contains address of format string
+    MOV R1, SP              @ move SP to R1 to store entry on stack
+    BL scanf                @ call scanf
+    LDR R0, [SP]            @ load value at SP into R0
+    ADD SP, SP, #4          @ restore the stack pointer
+    POP {PC}                @ return
+ 
+ 
 .data
 
 .balign 4
 a:              .skip       40
+format_str:     .asciz      "%d"
 printf_str:     .asciz      "array_a[%d] = %d\n"
 exit_str:       .ascii      "Terminating program.\n"
